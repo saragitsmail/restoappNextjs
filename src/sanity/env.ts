b9d7d9984d348f8c5b137a54c4e3ae4dd97aae69
saragitsmail <sara.smai:@fgei.ummto.dz>
2026-08-04 16:12:39 +0100

    fix token

--- src/sanity/env.ts
+++ src/sanity/env.ts
@@ -7,5 +7,4 @@ export const dataset =
 export const projectId =
   process.env.NEXT_PUBLIC_SANITY_PROJECT_ID || '9mr6co1a'
 
-export const token =
-  process.env.SANITY_API_TOKEN || 'skhJVU5o5H7BlcN5BagTbxXKVeN3tSzIeY2LJscC5aiKr1DCXuRt6FfswLLEmymPNaKVNveQaiJizs6jWMsvnLJnMh6MrX3zzN5negVW6q1DMYiyJtfxHFz0FdOXGMjTuZvHJLEytSeH9lEtSAzR1Sz3QEH9cgWYm77glFgb6US3jNzCtlrI'
+export const token = process.env.SANITY_API_TOKEN
